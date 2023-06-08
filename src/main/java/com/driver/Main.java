1474b1b8a2d5ffa332c901ccd3b320b05d818ea0
Harit <haritshandilya.beas@gmail.com>
2023-06-08 23:32:09 +0530

    Assesment Submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(10,10));
+        System.out.println(p.product(10,10,30));
+        System.out.println(p.product(10.5,10.2));
+    }
+}
+class Product{
+    public int product(int x, int y){
+        return x*y;
+    }
+
+    public int product(int x, int y, int z){
+        return x*y*z;
+    }
+
+    public double product(double x,double y){
+        return x*y;
+    }
 }
