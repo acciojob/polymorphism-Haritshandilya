0844cd04fa326b28b440d0f3cf5d4dc0e04bf7a7
Harit <haritshandilya.beas@gmail.com>
2023-06-09 22:56:00 +0530

    Assignment Submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,27 @@
 package com.driver;
 
 public class Main {
-    static class Product{
-        public int product(int x, int y){
-            return x*y;
-        }
-
-        //overloading
-        public int product(int x, int y, int z){
-            return x*y*z;
-        }
-
-        //overloading
-        public double product(double x,double y){
-            return x*y;
-        }
-    }
 
     public static void main(String[] args) {
         Product p = new Product();
-        System.out.println(p.product(10,10));
+        int result1 = p.product(10,10);
+        System.out.println(result1);
         System.out.println(p.product(10,10,30));
         System.out.println(p.product(10.5,10.2));
+    }
+}
+class Product{
+    public int product(int x, int y){
+        return x*y;
+    }
+
+    //overloading
+    public int product(int x, int y, int z){
+        return x*y*z;
+    }
+
+    //overloading
+    public double product(double x,double y){
+        return x*y;
     }
 }
